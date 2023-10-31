bff92c6f1b9294f8a9ed6932a7c19668a0240d70
Felipe Cahuasiri Ance <105305223+Cahuasiri@users.noreply.github.com>
2023-10-31 14:35:17 -0400

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'LARVENDER KS102 Drone',
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '84631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '85633',
+        paymentStatus: 'Paid',
+        shipping: 'Declivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'pending'
+    }
+]
